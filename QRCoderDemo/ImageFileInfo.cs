b5d2f3972e2d80a2c6f32763c0312f5b84ad3075
yangfan <yangfan@DESKTOP-FQD7G1K>
2022-08-18 00:21:40 +0800

    添加项目文件。

---
+++ QRCoderDemo/ImageFileInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCoderDemo
+{
+    public class ImageFileInfo
+    {
+        public string Name { get; set; }
+        public BitmapImage Source { get; set; }
+
+    }
+}
